d08915332aee47c32b3ce49a8878b1fc02252aff
Hardik Sharma <sharmahardikaz@gmail.com>
2023-09-02 23:27:27 +0530

    Connected with mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
+const mongoose=require("mongoose");
 
 const app=express();
 
@@ -9,6 +10,15 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({
     extended:true
 }))
+
+mongoose.connect("mongodb://localhost:27017/usersAuthDB",{useNewUrlParser:true})
+
+const userSchema={
+    email:String,
+    password:String
+}
+
+const User=new mongoose.model("User",userSchema);
 
 app.get("/",function(req,res){
     res.render("home");
@@ -21,6 +31,33 @@ app.get("/login",function(req,res){
 app.get("/register",function(req,res){
     res.render("register");
 });
+
+app.post("/register",function(req,res){
+    const newUser=new User({
+        email:req.body.username,
+        password:req.body.password
+    })
+
+    newUser.save().then(()=>{
+        res.render("secrets");
+    });
+})
+
+app.post("/login",function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    User.findOne({email:username}).exec().then(foundUser=>{
+        if(foundUser.password===password)
+        {
+            res.render("secrets");
+        }
+        else
+        {
+            console.warn("Data Not found");
+        }
+    }).catch(err=>console.log("Kuch bhi Dal rhe"));
+})
 
 app.listen(3000,function(){
     console.log("Server started on port 3000.");

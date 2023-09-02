7596aa65f9f2cea6e7ea36c9f54a72cbb325d38a
Hardik Sharma <sharmahardikaz@gmail.com>
2023-09-03 00:17:52 +0530

    Added mongoose encryption

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
+const encrypt=require("mongoose-encryption");
 
 const app=express();
 
@@ -13,10 +14,13 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/usersAuthDB",{useNewUrlParser:true})
 
-const userSchema={
+const userSchema=new mongoose.Schema({
     email:String,
     password:String
-}
+});
+
+const secret ="Thisisourlittlesecret";
+userSchema.plugin(encrypt, { secret: secret, encryptFields: ["password"] });
 
 const User=new mongoose.model("User",userSchema);
 
@@ -56,7 +60,8 @@ app.post("/login",function(req,res){
         {
             console.warn("Data Not found");
         }
-    }).catch(err=>console.log("Kuch bhi Dal rhe"));
+    }).catch(err=>{console.log("Kuch bhi Dal rhe");
+console.log(err);});
 })
 
 app.listen(3000,function(){
